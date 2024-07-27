dea9dab14bfd326901fb3fe2fc97a53c9b0bbd27
sinconorM <sinconormabelane@gmail.com>
2024-07-27 16:48:31 +0200

    [#16] Add goodbye method to Greeter.java

--- tut0/Greeter.java
+++ tut0/Greeter.java
@@ -8,6 +8,7 @@ public class Greeter {
             String name = getName();
             greeter(name);
         }
+	// Placeholder for calling goodbye() if needed in the future
     }
 
     public static String getName() {
@@ -18,6 +19,10 @@ public class Greeter {
 
     public static void greeter(String name) {
         System.out.println("Hello, " + name + "! We are thrilled to have you with us. ");
+    }
+
+    public static void goodbye() {
+        System.out.println("Goodbye! It was great having you here. We hope to see you again soon!");
     }
 }
 
