30e183270aa5d75c948d9bd97e7b62acaccc8211
sinconorM <sinconormabelane@gmail.com>
2024-07-27 15:28:20 +0200

    [#6] Add greeter(String name) function with longer message

--- tut0/Greeter.java
+++ tut0/Greeter.java
@@ -1,5 +1,12 @@
 public class Greeter {
     public static void main(String[] args) {
-        // Empty main function
+        if (args.length > 0) {
+            greeter(args[0]);
+        } else {
+            System.out.println("Please provide a name as a command line argument.");
+        }
+    }
+    public static void greeter(String name) {
+        System.out.println("Hello, " + name + "! We are thrilled to have you with us. ");
     }
 }
