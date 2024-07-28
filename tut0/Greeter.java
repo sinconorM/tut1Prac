d639760e5a1f3d64f2ebd20e63bb702ef9b05008
Kgwadimabushi Sinconor Mabelane <25927442@sun.ac.za>
2024-07-28 16:05:23 +0200

    yebo

--- tut0/Greeter.java
+++ tut0/Greeter.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Greeter {
     public static void main(String[] args) {
-        
+        System.out.println(getName());
+    }
+
+    public static String getName() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        return scanner.nextLine();
     }
 }
 
