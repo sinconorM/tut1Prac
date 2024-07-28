5d67443a3f4919f47610532b5bab211efd24337a
Kgwadimabushi Sinconor Mabelane <25927442@sun.ac.za>
2024-07-28 16:02:08 +0200

    yeah

--- tut0/Greeter.java
+++ tut0/Greeter.java
@@ -1,6 +1,17 @@
 public class Greeter {
     public static void main(String[] args) {
-        
+        if (args.length > 0) {
+            greeter(args[0]);
+        } else {
+            String name = getName();
+            greeter(name);
+        }
+    }
+
+    public static void greeter(String name) {
+        System.out.println("Hello, " + name + "! We are thrilled to have you with us. " +
+                           "Welcome to our community. We hope you have a fantastic experience here, " +
+                           "learn a lot, and make great connections. Enjoy your journey!");
     }
 }
 
