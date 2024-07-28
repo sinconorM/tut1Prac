fe11e2db55e2ebe382be95e016f3fac83fde5e8c
Kgwadimabushi Sinconor Mabelane <25927442@sun.ac.za>
2024-07-28 16:05:23 +0200

    rebase done and conflict resolved

--- tut0/Greeter.java
+++ tut0/Greeter.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Greeter {
     public static void main(String[] args) {
+
         if (args.length > 0) {
             greeter(args[0]);
         } else {
@@ -12,6 +15,14 @@ public class Greeter {
         System.out.println("Hello, " + name + "! We are thrilled to have you with us. " +
                            "Welcome to our community. We hope you have a fantastic experience here, " +
                            "learn a lot, and make great connections. Enjoy your journey!");
+
+        System.out.println(getName());
+    }
+
+    public static String getName() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        return scanner.nextLine();
     }
 }
 
