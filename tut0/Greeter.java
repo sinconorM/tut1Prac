82cdfefa95eeba51c4a0ab34fb4611920fe8ddc2
Kgwadimabushi Sinconor Mabelane <25927442@sun.ac.za>
2024-07-28 16:08:38 +0200

    yy

--- tut0/Greeter.java
+++ tut0/Greeter.java
@@ -1,6 +1,10 @@
 public class Greeter {
     public static void main(String[] args) {
-        
+    	goodbye();   
+    }
+
+    public static void goodbye() {
+        System.out.println("Goodbye! It was great having you here. We hope to see you again soon!");
     }
 }
 
